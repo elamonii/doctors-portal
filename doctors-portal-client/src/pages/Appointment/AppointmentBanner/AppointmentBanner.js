8834c083b78b2ccf9edb7a9def4a9f54fb4e4716
elamonii <75824927+elamonii@users.noreply.github.com>
2022-11-19 23:53:23 +0600

    calender

--- doctors-portal-client/src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ doctors-portal-client/src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BannerImage from '../../components/BannerImage/BannerImage';
 import BannerTransparent from '../../components/BannerImage/BannerTransparent';
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+
     return (
-        <header>
+        <header className='my-6'>
             <div className="hero">
                 <BannerTransparent></BannerTransparent>
 
                 <div className="hero-content flex-col lg:flex-row-reverse">                    
                     <BannerImage></BannerImage>
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                    <div className='flex justify-center text-center mr-6 w-[100%] lg:w-[50%] md:w-[50%]'>
+                        <div>
+                            <DayPicker
+                                mode="single"
+                                selected={selectedDate}
+                                onSelect={setSelectedDate}
+                            />
+                            <p>You have selected date: {format(selectedDate, 'PP')}</p>
+                        </div>
                     </div>
                 </div>
             </div>            
--- doctors-portal-client/src/pages/components/BannerImage/BannerImage.js
+++ doctors-portal-client/src/pages/components/BannerImage/BannerImage.js
@@ -3,7 +3,7 @@ import chair from '../../../assets/images/chair.png';
 
 const BannerImage = () => {
     return (
-        <img src={chair}  className="rounded-lg shadow-2xl w-[100%] lg:w-[50%]" alt='' />
+        <img src={chair}  className="rounded-lg shadow-2xl w-[100%] lg:w-[50%] md:w-[50%]" alt='' />
     );
 };
 
